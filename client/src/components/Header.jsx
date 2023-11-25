aee9946217bdbdaa7ce57aa40006c59d7ccf92e1
beth deutmeyer <bethdeutmeyer@gmail.com>
2023-11-24 18:36:10 -0600

    tooltips working, wrote about, imported fonts

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,8 @@
 function Header() {
     return (
         <div>
-            <h1>Beth Deutmeyer</h1>
+            <h1 id="name-header">Beth Deutmeyer</h1>
+            <h6>Full-stack web developer | Teacher | Musician | Crocheter | Word game enthusiast | Puzzle lover | Problem solver | Mom</h6>
         </div>
     )
 }
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom'
+
 export default function About() {
     return (
         <>
-        <h3>Welcome!</h3>
-        <h4>About Me</h4>
-        <p>I am a newly minted coder with imminent certification in full stack web development from the Northwestern University Coding Boot Camp. I have skills in a whole host of web development technologies and coding languages (see my resume for more details!), and I am eager to continue learning more through the boot camp and beyond.
+            <h2>Welcome!</h2>
             <br />
-            My background is in music, and I have had a successful career as a voice teacher for over 15 years. My training in the arts has proven to be an excellent foundation for learning to code; the fundamentals of breaking a big thing down into small parts, 
-        </p>
+            <h4 className="text-start">About Me</h4>
+            <p className="text-start">I am a newly minted coder with imminent certification in full stack web development from the Northwestern University Coding Boot Camp. I have skills in a whole host of web development technologies and coding languages (see my <Link className='text-decoration-none text-black hidden-link' to="/resume">resume page</Link> for more details!), and I am eager to continue learning more through the boot camp and beyond.</p>
+            <p className="text-start">I have spent over 15 years building a successful career as a voice teacher. I maintain a private studio at home, and have also taught at a few area high schools, colleges, and universities. I hope that my career journey will allow me to marry up my teaching skills with my new tech chops. Working to help people navigate this complex ecosystem and learn new and better ways to achieve their goals sounds incredibly rewarding. No matter what else the future holds for me, I intend to keep building cool sites, solving problems, untangling puzzles, and growing in my knowledge!</p>            
         </>
-           ); 
+    );
 };
