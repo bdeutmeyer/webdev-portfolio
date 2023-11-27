437e8b596eb1bf522a82d672b2d8b85a723d37eb
beth deutmeyer <bethdeutmeyer@gmail.com>
2023-11-27 14:55:10 -0600

    deleted bulky version

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -1,16 +1,16 @@
 // for future additions: all card images are cropped to 18:11 aspect ratio
-// import { Container, Row } from 'reactstrap';
-// import Projects from './Projects'
-// import portfolioProjects from '../../data/portfolioProjects';
+import { Container, Row } from 'reactstrap';
+import Projects from './Projects'
+import portfolioProjects from '../../data/portfolioProjects';
 
-// export default function Portfolio() {
-//     return (
-//         <>
-//             <Container fluid>
-//                 <Row lg="3" md="2" sm="1" xs="1" className='d-flex justify-content-around p-2 bg-dark pt-4 pb-4'>
-//                     <Projects portfolioProjects={portfolioProjects} />
-//                 </Row>
-//             </Container>
-//         </>
-//     );
-// };
+export default function Portfolio() {
+    return (
+        <>
+            <Container fluid>
+                <Row lg="3" md="2" sm="1" xs="1" className='d-flex justify-content-around p-2 bg-dark pt-4 pb-4'>
+                    <Projects portfolioProjects={portfolioProjects} />
+                </Row>
+            </Container>
+        </>
+    );
+};
