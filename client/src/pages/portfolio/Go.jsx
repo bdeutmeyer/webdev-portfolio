673679637b5e61211280095df13da3d36980a252
beth deutmeyer <bethdeutmeyer@gmail.com>
2023-11-27 09:28:36 -0600

    trying to deploy again

--- client/src/pages/portfolio/Go.jsx
+++ client/src/pages/portfolio/Go.jsx
@@ -3,10 +3,15 @@ import gameOnImg from '../../assets/images/GAME-ON-screenshot.png';
 import ghLogo from '../../assets/logos/github-mark.svg';
 import infoI from '../../assets/logos/info-solid.svg';
 import { Card, CardImg, CardImgOverlay, CardTitle, CardLink, Col, Tooltip } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 
 export default function GameOn() {
-    const [gameOnTooltip, setGameOnTooltip] = useState(false);
-    const toggleGO = () => setGameOnTooltip(!gameOnTooltip);
+    // const [gameOnTooltip, setGameOnTooltip] = useState(false);
+    // const toggleGo = () => setGameOnTooltip(!gameOnTooltip);
+    // const [goHover, setGoHover] = useState(false);
+    
+    const [goModal, setGoModal] = useState(false);
+    const toggleGo = () => setGoModal(!goModal);
     const [goHover, setGoHover] = useState(false);
 
     return (
@@ -28,17 +33,25 @@ export default function GameOn() {
                     <CardLink href="https://github.com/vladesovc/GAME-ON" className='col w-25'>
                         <img src={ghLogo} alt="GitHub Icon" />
                     </CardLink>
-                    <a href="#" id="aboutGameOn" className='col w-25'>
+                    {/* <a href="#" id="aboutGameOn" className='col w-25'>
                         <img src={infoI} alt="Info" className='w-25' /></a>
                     <Tooltip
                         autohide
                         isOpen={gameOnTooltip}
                         target="aboutGameOn"
-                        toggle={toggleGO}
+                        toggle={toggleGo}
                         data-bs-delay='{"show": 0, "hide":3000}'
                     >
                         GAME ON! serves as a portal to free online games. Users can see all available games, search by title, search by genre, and access direct links to each game for immediate play. If logged in, they can save games to their user dashboard. The site was a group endeavor; my teammates were <a href="https://github.com/vladesovc" className="hidden-link text-white">Corey Vladesov</a>, <a href="https://github.com/lizfischstix" className="hidden-link text-white">Liz Fischenich</a>, and <a href="https://github.com/taycannon" className="hidden-link text-white">Taylor Cannon.</a>.
-                    </Tooltip>
+                    </Tooltip> */}
+                                        <a href="#" id="aboutGameOn" className='col w-25' onClick={toggleGo}>
+                        <img src={infoI} alt="Info" className='w-25' /></a>
+                        <Modal isOpen={goModal} toggle={toggleGo}>
+                                        <ModalHeader toggle={toggleGo}>About the GAME ON site</ModalHeader>
+                                        <ModalBody>
+                                        GAME ON! serves as a portal to free online games. Users can see all available games, search by title, search by genre, and access direct links to each game for immediate play. If logged in, they can save games to their user dashboard. The site was a group endeavor; my teammates were <a href="https://github.com/vladesovc" className="hidden-link">Corey Vladesov</a>, <a href="https://github.com/lizfischstix" className="hidden-link">Liz Fischenich</a>, and <a href="https://github.com/taycannon" className="hidden-link">Taylor Cannon</a>.
+                                        </ModalBody>
+                                    </Modal>
                     </div>
                 </CardImgOverlay> : <CardImgOverlay id="aboutGameOn"></CardImgOverlay>}
                 </div>
