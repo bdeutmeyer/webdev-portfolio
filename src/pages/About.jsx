7fadba527d44eb4a1a64c3f9e78ba4336873d721
beth deutmeyer <bethdeutmeyer@gmail.com>
2024-07-15 14:33:47 -0500

    updated about page and resume

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,8 +10,8 @@ export default function About() {
             <br />
             <div>
             <img src={headshot} alt="Beth Deutmeyer headshot" className="float-start me-3 rounded shadow" width="15%"/>
-            <p className="text-start">I am a newly minted coder with recent certification in full stack web development from the Northwestern University Coding Boot Camp. I have skills in a whole host of web development technologies and coding languages (see my <Link className='text-decoration-none text-black hidden-link' to="/resume">resume page</Link> for more details!), and I am eager to continue learning more through the boot camp and beyond.</p>
-            <p className="text-start">I have spent over 15 years building a successful career as a voice teacher. I maintain a private studio at home, and have also taught at a few area high schools, colleges, and universities. I hope that my career journey will allow me to marry up my love of teaching with my new tech chops. Working to help people learn new and better ways to achieve their goals through technology sounds incredibly rewarding. No matter what else the future holds for me, I intend to keep building cool sites, solving problems, untangling puzzles, and growing in my knowledge!</p>     
+            <p className='text-start'>Welcome!</p>
+            <p className="text-start">I am an experienced teacher and accomplished musician with additional training in web development. After limiting myself to part-time work for several years in order to raise my children at home, I am looking to expand my career. Responsible, highly educated, and versatile, I am eager to branch out into new arenas, further my skills, and leverage my abilities in interesting and challenging ways!</p>     
             </div>
        <br />
        <br />
